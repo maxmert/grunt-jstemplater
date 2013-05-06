a1ce54c1d1dcc4e09a42b1de46511d404615eccf
Vetrenko Maxim <vetrenko.maxim@gmail.com>
2013-05-06 18:40:14 +0400

    Last fix for 0.4

--- tasks/template.js
+++ tasks/template.js
@@ -1,42 +1,17 @@
 module.exports = function(grunt) {
 
-	var helpers = require('grunt-lib-legacyhelpers').init(grunt);
-	
-	// ==========================================================================
-	// TASKS
-	// ==========================================================================
-
-	grunt.registerMultiTask('template', 'Concatenate all javascript templates into json.', function() {
-		var files = grunt.file.expandFiles(this.file.src);
-		var staticPath = this.data.variables.staticPath ? this.data.variables.staticPath : 'javascripts/templates/';
-		staticPath.charAt( staticPath.length - 1 ) !== '/' ? staticPath += '/' : void 0;
-		var src = grunt.helper('template', files, staticPath);
-		var obj = {};
-		var name = 'TEMPLATES';
-		
-		for( item in src ) {
-			obj = deepmerge(obj, JSON.parse(src[item]));
-		}
-		
-		if( this.data.variables.name && this.data.variables.name !== '' )
-			name = this.data.variables.name
-		
-		grunt.file.write(this.file.dest, 'var '+ name +' = ' + JSON.stringify(obj) + ';');
-
-		grunt.log.ok(this.file.dest, name);
-	});
 
 	// ==========================================================================
 	// HELPERS
 	// ==========================================================================
 
-		helpers.name('template', function(files, staticPath) {
+		function template(files, staticPath) {
 			return files ? files.map(function(filepath){
 				paths = filepath.split(staticPath)[1].split('/')
 				jsonpart = buildJsonPath(paths, filepath);
 				return jsonpart;
 			}) : ''
-		});
+		};
 
 
 	// ==========================================================================
@@ -73,10 +48,35 @@ module.exports = function(grunt) {
 				res += '"' + node + '":' + buildJsonPath(path, filepath) + '}'
 			}
 			else if( path.length == 0 ) {
-				res += '"' + node[0].match(/(.*)\.[^.]+$/)[1] + '":"' + grunt.task.directive(filepath, grunt.file.read).replace(/\t/g,"").replace(/ /g,"").replace(/\n/g," ").replace(/"/g,'\\"').replace(/'/g,'\\"') + '"}'
+				res += '"' + node[0].match(/(.*)\.[^.]+$/)[1] + '":"' + grunt.file.read(filepath).replace(/\t/g,"").replace(/\n/g," ").replace(/"/g,'\\"').replace(/'/g,'\\"') + '"}'
 			}
 			return res;
 		}
+
+
+	// ==========================================================================
+	// TASKS
+	// ==========================================================================
+
+	grunt.registerMultiTask('template', 'Concatenate all javascript templates into json.', function() {
+		var files = grunt.file.expand(this.filesSrc);
+		var staticPath = this.data.variables.staticPath ? this.data.variables.staticPath : 'javascripts/templates/';
+		staticPath.charAt( staticPath.length - 1 ) !== '/' ? staticPath += '/' : void 0;
+		var src = template(files, staticPath);
+		var obj = {};
+		var name = 'TEMPLATES';
+		
+		for( item in src ) {
+			obj = deepmerge(obj, JSON.parse(src[item]));
+		}
+		
+		if( this.data.variables.name && this.data.variables.name !== '' )
+			name = this.data.variables.name
+		
+		grunt.file.write(this.data.dest, 'var '+ name +' = ' + JSON.stringify(obj) + ';');
+
+		grunt.log.ok(this.data.dest, name);
+	});
 
 };
 
